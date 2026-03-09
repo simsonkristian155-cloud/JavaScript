c872980d7334e29b5edd68c033a79371519eb5cf
Simson Kristian <simsonkristian155@gmail.com>
2026-03-09 16:00:08 +0700

    final javascript assignment with header, nim, photo and error trapping

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ let nama = document.getElementById("nama").value;
 let jumlah = document.getElementById("jumlah").value;
 let container = document.getElementById("container");
 
-// ERROR TRAPPING
 if(nama == ""){
 alert("Nama tidak boleh kosong!");
 return;
@@ -34,6 +33,7 @@ container.innerHTML += `<button onclick="buatRadio()">OK</button>`;
 }
 
 
+
 function buatRadio(){
 
 let jumlah = document.getElementById("jumlah").value;
@@ -47,7 +47,6 @@ for(let i = 1; i <= jumlah; i++){
 
 let teks = document.getElementById("pilihan"+i).value;
 
-// ERROR TRAPPING
 if(teks == ""){
 alert("Semua pilihan harus diisi!");
 return;
@@ -68,6 +67,7 @@ html += `<br><button onclick="inputEmail()">OK</button>`;
 container.innerHTML += html;
 
 }
+
 
 
 function inputEmail(){
@@ -92,6 +92,7 @@ container.innerHTML += `
 }
 
 
+
 function cekEmail(){
 
 let email = document.getElementById("email").value;
@@ -99,13 +100,14 @@ let email = document.getElementById("email").value;
 let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 if(!email.match(pattern)){
-alert("Email tidak valid, masukkan ulang!");
+alert("Email tidak valid!");
 return;
 }
 
 tampilkanHasil();
 
 }
+
 
 
 function tampilkanHasil(){
@@ -120,8 +122,8 @@ let pilihanDipilih = pilih.value;
 
 document.body.innerHTML = `
 <h2>
-Hallo, nama saya ${nama}, email ${email}. <br>
-Saya mempunyai sejumlah ${jumlah} pilihan yaitu ${pilihanArray.join(", ")} <br>
+Hallo, nama saya ${nama}, email ${email}. <br><br>
+Saya mempunyai sejumlah ${jumlah} pilihan yaitu ${pilihanArray.join(", ")} <br><br>
 dan saya memilih ${pilihanDipilih}.
 </h2>
 `;
