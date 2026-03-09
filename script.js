570da1f41e00dfd2124a7d319cd34df061294d38
Simson Kristian <simsonkristian155@gmail.com>
2026-03-09 14:57:44 +0700

    generate pilihan input menggunakan loop

---
+++ script.js
@@ -0,0 +1,20 @@
+function buatPilihan(){
+
+let jumlah = document.getElementById("jumlah").value;
+let container = document.getElementById("container");
+
+container.innerHTML = "";
+
+for(let i = 1; i <= jumlah; i++){
+
+container.innerHTML += `
+<label>Pilihan ${i} :</label>
+<input type="text" id="pilihan${i}">
+<br><br>
+`;
+
+}
+
+container.innerHTML += `<button onclick="buatRadio()">OK</button>`;
+
+}
